16098286645ce46580f77da4909b3750d3202699
Mihir20022000 <48185945+Mihir20022000@users.noreply.github.com>
2020-05-17 14:39:01 +0530

    updated version

--- assest/javascript/end.js
+++ assest/javascript/end.js
@@ -10,21 +10,19 @@ const Max_highScores = 10;
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 username.addEventListener("keyup" , function(){
     saveScorebtn.disabled = !username.value;
-})
+});
 
 function saveHighScore(e){
     console.log(username.value);
     e.preventDefault();
 
-    const score = {
-        
+    const score = {     
         score: mostRecentScore,
         name: username.value
-
     };
     highScores.push(score);
     highScores.sort((a,b) => a.score - b.score);
     highScores.splice(10);
     localStorage.setItem("highScores", JSON.stringify(highScores));
-    console.log(highScores);
+    window.location.assign('/');
 };
